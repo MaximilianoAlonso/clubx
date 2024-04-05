4bf03b92c15e621a994ee8a13e46db362ee596b5
Maximiliano <elgrungemaxi1986@gmail.com>
2024-04-04 23:56:54 -0300

    arreglo menu

--- app.js
+++ app.js
@@ -22,6 +22,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+const axios = require('axios');
+
+async function obtenerIPPublica() {
+  try {
+    const respuesta = await axios.get('https://api.ipify.org/?format=json');
+    const ipPublica = respuesta.data.ip;
+    console.log('La dirección IP pública es:', ipPublica);
+    return ipPublica;
+  } catch (error) {
+    console.error('Hubo un error al obtener la dirección IP pública:', error);
+    return null;
+  }
+}
+
+obtenerIPPublica();
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
